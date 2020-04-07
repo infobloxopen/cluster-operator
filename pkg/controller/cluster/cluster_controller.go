b13fb056fd08f6b059205e9bc416f2a7e189e4e3
hryan-ib <hryan@infoblox.com>
2020-04-07 13:47:06 -0700

    working on update cycle

--- pkg/controller/cluster/cluster_controller.go
+++ pkg/controller/cluster/cluster_controller.go
@@ -5,6 +5,7 @@ import (
 
 	"context"
 	"os"
+	"time"
 
 	"github.com/infobloxopen/cluster-operator/kops"
 	clusteroperatorv1alpha1 "github.com/infobloxopen/cluster-operator/pkg/apis/clusteroperator/v1alpha1"
@@ -23,9 +24,11 @@ import (
 	//"k8s.io/kops/cmd/kops"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
@@ -51,8 +54,38 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 		return err
 	}
 
+	pred := predicate.Funcs{
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			// var oldInstance clusteroperatorv1alpha1.Cluster
+			// obj, _ := json.Marshal(e.ObjectOld)
+			// err := json.Unmarshal(obj, &oldInstance)
+			// if err != nil {
+			// 	fmt.Print("error")
+			// }
+
+			// var newInstance clusteroperatorv1alpha1.Cluster
+			// obj, _ = json.Marshal(e.ObjectOld)
+			// err = json.Unmarshal(obj, &newInstance)
+			// if err != nil {
+			// 	fmt.Print("error")
+			// }
+			// // clusterFinalizer := "cluster.finalizer.cluster-operator.infobloxopen.github.com"
+			// if newInstance.Status.Phase != oldInstance.Status.Phase {
+			// 	// (utils.Contains(e.MetaNew.GetFinalizers(), clusterFinalizer)  {
+			// 	fmt.Print("me here\n")
+			// 	return false
+			// }
+
+			if e.MetaNew.GetGeneration() == e.MetaOld.GetGeneration() {
+				return false
+			}
+
+			return true
+		},
+	}
+
 	// Watch for changes to primary resource Cluster
-	err = c.Watch(&source.Kind{Type: &clusteroperatorv1alpha1.Cluster{}}, &handler.EnqueueRequestForObject{})
+	err = c.Watch(&source.Kind{Type: &clusteroperatorv1alpha1.Cluster{}}, &handler.EnqueueRequestForObject{}, pred)
 	if err != nil {
 		return err
 	}
@@ -109,24 +142,30 @@ func (r *ReconcileCluster) Reconcile(request reconcile.Request) (reconcile.Resul
 	clusterFinalizer := "cluster.finalizer.cluster-operator.infobloxopen.github.com"
 
 	kc := CheckKopsDefaultConfig(instance.Spec)
-
 	// If the cluster is not waiting for deletion, handle it normally
 	if instance.ObjectMeta.DeletionTimestamp.IsZero() {
+		//If the CR is being requeued but is in the DONE phase, it must need updating
+		// if instance.Status.Phase == clusteroperatorv1alpha1.ClusterDone {
+		// 	instance.Status.Phase = clusteroperatorv1alpha1.ClusterPending
+		// 	if err := r.client.Update(context.TODO(), instance); err != nil {
+		// 		return reconcile.Result{}, err
+		// 	}
+		// }
+
 		// If no phase set default to pending for the initial phase
 		if instance.Status.Phase == "" {
 			instance.Status.Phase = clusteroperatorv1alpha1.ClusterPending
 			instance.Spec.KopsConfig = CheckKopsDefaultConfig(instance.Spec)
-			if err := r.client.Update(context.TODO(), instance); err != nil {
-				return reconcile.Result{}, err
-			}
+			// if err := r.client.Update(context.TODO(), instance); err != nil {
+			// 	return reconcile.Result{}, err
+			// }
 		}
-
 		// Add the finalizer and update the object
 		if !utils.Contains(instance.ObjectMeta.Finalizers, clusterFinalizer) {
 			instance.ObjectMeta.Finalizers = append(instance.ObjectMeta.Finalizers, clusterFinalizer)
-			if err := r.client.Update(context.TODO(), instance); err != nil {
-				return reconcile.Result{}, err
-			}
+			// if err := r.client.Update(context.TODO(), instance); err != nil {
+			// 	return reconcile.Result{}, err
+			// }
 		}
 
 		tempConfigFile := instance.Spec.Name + ".yaml"
@@ -137,130 +176,142 @@ func (r *ReconcileCluster) Reconcile(request reconcile.Request) (reconcile.Resul
 
 		// Run State Machine
 		// PENDING -> SETUP -> DONE
-		switch instance.Status.Phase {
-		case clusteroperatorv1alpha1.ClusterPending:
-			reqLogger.Info("Phase: PENDING")
-			//creating cluster
-			replaceOptions := &kops.ReplaceOptions{Filenames: []string{"tmp/" + tempConfigFile}, Force: true}
+		// switch instance.Status.Phase {
+		// case clusteroperatorv1alpha1.ClusterPending:
+		reqLogger.Info("Phase: PENDING")
+		//creating cluster
+		replaceOptions := &kops.ReplaceOptions{Filenames: []string{"tmp/" + tempConfigFile}, Force: true}
 
-			factory := util.NewFactory(&util.FactoryOptions{RegistryPath: kc.StateStore})
-			err = kops.RunReplace(factory, os.Stdout, replaceOptions)
+		factory := util.NewFactory(&util.FactoryOptions{RegistryPath: kc.StateStore})
+		err = kops.RunReplace(factory, os.Stdout, replaceOptions)
 
-			if err != nil {
-				reqLogger.Error(err, "error creating cluster")
-				return reconcile.Result{}, err
-			}
-			reqLogger.Info("Cluster Created")
-			instance.Status.Phase = clusteroperatorv1alpha1.ClusterUpdate
-
-		case clusteroperatorv1alpha1.ClusterUpdate:
-			reqLogger.Info("Phase: UPDATE")
-
-			updateOptions := &kops.UpdateClusterOptions{}
-			updateOptions.InitDefaults()
-
-			factory := util.NewFactory(&util.FactoryOptions{RegistryPath: kc.StateStore})
-			_, err := kops.RunUpdateCluster(factory, kc.Name, os.Stdout, updateOptions)
-
-			if err != nil {
-				reqLogger.Error(err, "error updating cluster")
-				return reconcile.Result{}, err
-			}
-
-			//get kubeconfig
-			var mode os.FileMode = 509
-			err = os.MkdirAll("./tmp", mode)
-			if err != nil {
-				return reconcile.Result{}, err
-			}
-
-			_, err = os.Create("tmp/config-" + kc.Name)
-			if err != nil {
-				return reconcile.Result{}, err
-			}
-
-			configOptions := &kops.ExportKubecfgOptions{KubeConfigPath: "tmp/config-" + kc.Name}
-			clusterName := []string{kc.Name}
-
-			configFactory := util.NewFactory(&util.FactoryOptions{RegistryPath: kc.StateStore})
-			var config *clusteroperatorv1alpha1.KubeConfig
-			config, err = kops.RunExportKubecfg(configFactory, os.Stdout, configOptions, clusterName)
-			if err != nil {
-				return reconcile.Result{}, err
-			}
-
-			instance.Status.KubeConfig = *config
-			reqLogger.Info("KUBECONFIG Updated")
-
-			//rolling udpates
-			os.Setenv("KUBECONFIG", "tmp/config-"+kc.Name)
-
-			//TODO: Right now, using defaults for intervals. Need to make changable
-			rollingOptions := &kops.RollingUpdateOptions{ClusterName: kc.Name}
-			rollingOptions.InitDefaults()
-
-			factory = util.NewFactory(&util.FactoryOptions{RegistryPath: kc.StateStore})
-			err = kops.RunRollingUpdateCluster(factory, os.Stdout, rollingOptions)
-
-			// Some changes will require rebuilding the nodes (for example, resizing nodes or changing the AMI)
-			// We call rolling-update to apply these changes
-			// out, err = k.RollingUpdateCluster(GetKopsConfig(instance.Spec))
-			// reqLogger.Info(out)
-			if err != nil {
-				reqLogger.Error(err, "error performing rolling update on cluster")
-				return reconcile.Result{}, err
-			}
-			reqLogger.Info("Cluster Updated")
-			instance.Status.Phase = clusteroperatorv1alpha1.ClusterSetup
-		case clusteroperatorv1alpha1.ClusterSetup:
-			reqLogger.Info("Phase: SETUP")
-
-			// Setenv required if not using default .kube/config,
-			// the --kubeconfig option does not currently work for kops validate (1.18.2-alpha2)
-			os.Setenv("KUBECONFIG", "tmp/config-"+kc.Name)
-			validateOptions := &kops.ValidateClusterOptions{Kubeconfig: "tmp/config-" + kc.Name}
-			validateOptions.InitDefaults()
-			clusterName := []string{kc.Name}
-
-			factory := util.NewFactory(&util.FactoryOptions{RegistryPath: kc.StateStore})
-			status, err := kops.RunValidateCluster(factory, clusterName, os.Stdout, validateOptions)
-
-			if err != nil {
-				return reconcile.Result{}, err
-			}
-			instance.Status.KopsStatus = clusteroperatorv1alpha1.KopsStatus{}
-			if len(status.Failures) > 0 {
-				instance.Status.KopsStatus.Failures = status.Failures
-				reqLogger.Info("Cluster Not Ready")
-			} else if len(status.Nodes) > 0 {
-				instance.Status.KopsStatus.Nodes = status.Nodes
-				reqLogger.Info("Cluster Created")
-				instance.Status.Phase = clusteroperatorv1alpha1.ClusterDone
-			} else {
-				// FIXME - If we get this state try validate again!!!
-				reqLogger.Info("Validate Returned Unexpected Result")
-			}
-		case clusteroperatorv1alpha1.ClusterDone:
-			reqLogger.Info("Phase: DONE")
-			return reconcile.Result{}, nil
-		default:
-			reqLogger.Info("NOP")
-			return reconcile.Result{}, nil
+		if err != nil {
+			reqLogger.Error(err, "error creating cluster")
+			return reconcile.Result{}, err
 		}
+		reqLogger.Info("Cluster Config Update")
+		// instance.Status.Phase = clusteroperatorv1alpha1.ClusterUpdate
 
-		// Update the Cluster instance, setting the status to the respective phase:
+		// case clusteroperatorv1alpha1.ClusterUpdate:
+		instance.Status.Phase = clusteroperatorv1alpha1.ClusterUpdate
 		if err := r.client.Status().Update(context.TODO(), instance); err != nil {
 			return reconcile.Result{}, err
 		}
+		reqLogger.Info("Phase: UPDATE")
 
-		if instance.Status.Phase == clusteroperatorv1alpha1.ClusterSetup {
-			// TODO - Set to time.Duration depending on back-off behavior
-			//return reconcile.Result{RequeueAfter: time.Minute}, nil
-			return reconcile.Result{Requeue: true}, nil
+		updateOptions := &kops.UpdateClusterOptions{}
+		updateOptions.InitDefaults()
+
+		factory = util.NewFactory(&util.FactoryOptions{RegistryPath: kc.StateStore})
+		_, err := kops.RunUpdateCluster(factory, kc.Name, os.Stdout, updateOptions)
+
+		if err != nil {
+			reqLogger.Error(err, "error updating cluster")
+			return reconcile.Result{}, err
 		}
 
+		//get kubeconfig
+		var mode os.FileMode = 509
+		err = os.MkdirAll("./tmp", mode)
+		if err != nil {
+			return reconcile.Result{}, err
+		}
+
+		_, err = os.Create("tmp/config-" + kc.Name)
+		if err != nil {
+			return reconcile.Result{}, err
+		}
+
+		configOptions := &kops.ExportKubecfgOptions{KubeConfigPath: "tmp/config-" + kc.Name}
+		clusterName := []string{kc.Name}
+
+		configFactory := util.NewFactory(&util.FactoryOptions{RegistryPath: kc.StateStore})
+		var config *clusteroperatorv1alpha1.KubeConfig
+		config, err = kops.RunExportKubecfg(configFactory, os.Stdout, configOptions, clusterName)
+		if err != nil {
+			return reconcile.Result{}, err
+		}
+
+		instance.Status.KubeConfig = *config
+		reqLogger.Info("KUBECONFIG Updated")
+
+		//rolling udpates
+		os.Setenv("KUBECONFIG", "tmp/config-"+kc.Name)
+
+		//TODO: Right now, using defaults for intervals. Need to make changable
+		rollingOptions := &kops.RollingUpdateOptions{ClusterName: kc.Name}
+		rollingOptions.InitDefaults()
+
+		factory = util.NewFactory(&util.FactoryOptions{RegistryPath: kc.StateStore})
+		err = kops.RunRollingUpdateCluster(factory, os.Stdout, rollingOptions)
+
+		// Some changes will require rebuilding the nodes (for example, resizing nodes or changing the AMI)
+		// We call rolling-update to apply these changes
+		// out, err = k.RollingUpdateCluster(GetKopsConfig(instance.Spec))
+		// reqLogger.Info(out)
+		if err != nil {
+			reqLogger.Error(err, "error performing rolling update on cluster")
+			return reconcile.Result{}, err
+		}
+		reqLogger.Info("Cluster Updated")
+		instance.Status.Phase = clusteroperatorv1alpha1.ClusterSetup
+		if err := r.client.Status().Update(context.TODO(), instance); err != nil {
+			return reconcile.Result{}, err
+		}
+		// instance.Status.Phase = clusteroperatorv1alpha1.ClusterSetup
+		// case clusteroperatorv1alpha1.ClusterSetup:
+		reqLogger.Info("Phase: SETUP")
+
+		// Setenv required if not using default .kube/config,
+		// the --kubeconfig option does not currently work for kops validate (1.18.2-alpha2)
+		os.Setenv("KUBECONFIG", "tmp/config-"+kc.Name)
+		validateOptions := &kops.ValidateClusterOptions{Kubeconfig: "tmp/config-" + kc.Name}
+		validateOptions.InitDefaults()
+		// clusterName := []string{kc.Name}
+
+		factory = util.NewFactory(&util.FactoryOptions{RegistryPath: kc.StateStore})
+		status, err := kops.RunValidateCluster(factory, clusterName, os.Stdout, validateOptions)
+
+		// if err != nil {
+		// 	instance.Status.Phase = clusteroperatorv1alpha1.ClusterPending
+		// 	return reconcile.Result{}, err
+		// }
+		instance.Status.KopsStatus = clusteroperatorv1alpha1.KopsStatus{}
+		if err != nil {
+			reqLogger.Info("Cluster Not Ready")
+			// instance.Status.Phase = clusteroperatorv1alpha1.ClusterPending
+		} else if len(status.Nodes) > 0 {
+			instance.Status.KopsStatus.Nodes = status.Nodes
+			reqLogger.Info("Cluster Created")
+			instance.Status.Phase = clusteroperatorv1alpha1.ClusterDone
+			reqLogger.Info("Phase: DONE")
+			if err := r.client.Status().Update(context.TODO(), instance); err != nil {
+				return reconcile.Result{}, err
+			}
+			//requeues every ten minutes to make sure its synced if any manual changes were done
+			return reconcile.Result{RequeueAfter: time.Minute * 10}, nil
+		} else {
+			// FIXME - If we get this state try validate again!!!
+			reqLogger.Info("Validate Returned Unexpected Result")
+			// instance.Status.Phase = clusteroperatorv1alpha1.ClusterPending
+		}
+
+		// Update the Cluster instance, setting the status to the respective phase:
+		// if err := r.client.Update(context.TODO(), instance); err != nil {
+		// 	return reconcile.Result{}, err
+		// }
+
+		//It did not finish validating, requeue in five minutes
+		return reconcile.Result{RequeueAfter: time.Minute * 5}, nil
+
+		//if the phase is at PENDING here, that means it failed validation and will go through the process again
+		// if instance.Status.Phase == clusteroperatorv1alpha1.ClusterPending {
+		// 	// Will requeue in five minutes.
+		// 	return reconcile.Result{RequeueAfter: time.Minute * 1}, nil
+		// }
+
 		// Don't requeue. We should get called to reconcile when the CR changes.
-		return reconcile.Result{}, nil
+		// return reconcile.Result{}, nil
 
 	} else if utils.Contains(instance.ObjectMeta.Finalizers, clusterFinalizer) {
 		// our finalizer is present, so delete cluster first
@@ -278,7 +329,7 @@ func (r *ReconcileCluster) Reconcile(request reconcile.Request) (reconcile.Resul
 		// remove our finalizer from the list and update it.
 		instance.ObjectMeta.Finalizers = utils.Remove(instance.ObjectMeta.Finalizers, clusterFinalizer)
 
-		if err := r.client.Update(context.TODO(), instance); err != nil {
+		if err := r.client.Status().Update(context.TODO(), instance); err != nil {
 			return reconcile.Result{}, err
 		}
 
