b69c3aa990aa4aeddaa5c095f40b79b9ff1c64a7
Jenna Eizadi <jeizadi@gmail.com>
2020-05-12 17:34:47 -0700

    Cluster configuration from fork moved here

---
+++ cmd/manager/main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/spf13/viper"
+	"os"
+	"testing"
+)
+
+func TestConfig1(t *testing.T) {
+	host := "info"
+	os.Setenv(appEnvPrefix+"_METRICS_HOST", host)
+	if viper.GetString("metrics.host") != host {
+		t.Error("Expected", host, "Got", viper.GetString("metrics.host"))
+	}
+}
+
+func TestConfig2(t *testing.T) {
+	var port int32 = 42
+	os.Setenv(appEnvPrefix+"_METRICS_PORT", "42")
+	if viper.GetInt32("metrics.port") != port {
+		t.Error("Expected", port, "Got", viper.GetString("metrics.port"))
+	}
+}
